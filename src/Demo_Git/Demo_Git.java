c328268805639d899957e363d6c06cf68aa5181b
ywinster <Yesudoss.Winster@mnscorp.onmicrosoft.com>
2021-05-14 12:44:07 +0530

    I am committing new changes

--- src/Demo_Git/Demo_Git.java
+++ src/Demo_Git/Demo_Git.java
@@ -5,6 +5,10 @@ public class Demo_Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo_Git");
+			
 	}
-
+	public	void printer() {
+		System.out.println("I am learning git");
+		
+	}
 }
